0135b413ee3f5a452aa27206d9635186d5bbc63d
Iago López <ilopezsmx2011@gmail.com>
2024-02-01 11:27:17 +0100

    [FIX] suscribers offer

--- src/genweb6/tfemarket/content/offer/offer.py
+++ src/genweb6/tfemarket/content/offer/offer.py
@@ -447,6 +447,10 @@ class EditForm(edit.DefaultEditForm):
 
 def numOfferDefaultValue(offer, event):
     tfe_tool = genwebTfemarketConfig()
+
+    if not tfe_tool.enable_suscribers:
+        return
+
     center = tfe_tool.center_code
     total = tfe_tool.count_offers + 1
 
@@ -458,6 +462,10 @@ def numOfferDefaultValue(offer, event):
 
 
 def defineTeacherAsEditor(offer, event):
+    tfe_tool = genwebTfemarketConfig()
+    if not tfe_tool.enable_suscribers:
+        return
+
     creator = getattr(offer.getOwner(), '_id', None)
     teacher = offer.teacher_manager
 
